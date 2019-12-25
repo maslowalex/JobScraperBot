fa34de46960e5658bb970747e55ace7f9d6e5c3b
maslowalex <maslo.rails@gmail.com>
2019-12-25 17:26:54 +0200

    Added protection against banners

--- lib/periodical.ex
+++ lib/periodical.ex
@@ -3,9 +3,10 @@ defmodule Periodical do
     request = HTTPoison.get!("https://www.musicalchairs.info/#{instrument}/jobs")
     case request.status_code do
       200 ->
-        Floki.find(request.body, ".preview")
+        IO.inspect filter_banners(request.body)
+        filter_banners(request.body)
         |> Enum.reduce([], fn x, acc ->
-          acc ++ [{find_name(x), find_position(x), find_link(x)}]
+          acc ++ generate_row(x)
         end)
         |> insert_to_repo(instrument)
       _ ->
@@ -13,7 +14,17 @@ defmodule Periodical do
     end
   end
 
+  defp filter_banners(body) do
+    Floki.find(body, ".preview")
+    |> Enum.reject(fn x -> Floki.find(x, ".rev-container") != [] end)
+  end
+
+  defp generate_row(element) do
+    [{find_name(element), find_position(element), find_link(element)}]
+  end
+
   defp find_name(item) do
+    IO.inspect Floki.find(item, ".post_item_name")
     [{_, _, name}] = Floki.find(item, ".post_item_name")
 
     List.first(name)
