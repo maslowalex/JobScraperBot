f3d103b6ac5838638862a4c56b03bd5d4fc1ea11
maslowalex <maslo.rails@gmail.com>
2020-01-16 12:51:24 +0200

    fixed typo

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,7 @@ config :periodical, Periodical.Repo,
   database: "periodical_repo",
   username: "postgres",
   password: "postgres",
-  hostname: System.get_env("DATABASE_URL", "localhost"),
+  hostname: System.get_env("DATABASE_URL") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :periodical, ecto_repos: [Periodical.Repo]
